80c82046448bc0ba5a8e0fd90cc2098f55f1f77b
kero <kerlossony@yahoo.com>
2024-05-25 17:43:20 +0300

    Timer Worked

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -2,66 +2,63 @@ import { useEffect, useState } from "react"
 
 
 export default function Home() {
-    const [secounds, setSecounds] = useState(0)
-    const [minutes, setMinutes] = useState(0)
-    const [hours, setHours] = useState(0)
-    const [days, setDays] = useState(0)
-    const [startCount, setStartCount] = useState(false)
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(59);
+    const [seconds, setSeconds] = useState(55);
     const [intervalId, setIntervalId] = useState<any>(null);
     const [stopCounter, setStopCounter] = useState<any>(false)
-    const handleCounter = () => {
-        setStartCount(true)
-        const intervalId = setInterval(() => {
-            if (secounds === 59) {
-                setSecounds(0);
-                if (minutes === 59) {
-                    setMinutes(0);
-                    if (hours === 23) {
-                        setHours(0);
-                        setDays(prevDays => prevDays + 1);
-                    } else {
-                        setHours(prevHours => prevHours + 1);
-                    }
-                } else {
-                    setMinutes(prevMinutes => prevMinutes + 1);
-                }
-            } else {
-                setSecounds(prevSeconds => prevSeconds + 1);
-            }
-        }, 1000);
 
-        setIntervalId(intervalId);
+    const startTimer = () => {
+        setStopCounter(true)
+        setIntervalId(
+            setInterval(() => {
+                setSeconds((prevSeconds) => {
+                    const nextSeconds = prevSeconds + 1;
+                    if (nextSeconds === 60) {
+                        setMinutes(prevMinutes => prevMinutes + 1);
+                        return 0;
+                    }
+                    return nextSeconds;
+                });
+            }, 1000)
+        );
     }
+    const stopTimer = () => {
+        setStopCounter(false)
+        clearInterval(intervalId)
+    }
+
+    const resetCounter = () => {
+        clearInterval(intervalId)
+        setSeconds(0)
+        setStopCounter(false)
+    }
+
     useEffect(() => {
-        if (!stopCounter && startCount) {
-            handleCounter
-        } else {
-            clearInterval(intervalId)
+        return () => clearInterval(intervalId);
+    }, [intervalId]);
+
+    useEffect(() => {
+        if (minutes === 60) {
+            setHours(prevHours => prevHours + 1);
+            setMinutes(0);
         }
-    }, [secounds, minutes, hours, days]);
+    }, [minutes]);
+
     return (
         <div
             className="flex flex-col items-center justify-center h-[85vh] bg-black text-white"
         >
-            {days + ":" + hours + ":" + minutes + ":" + secounds}
+            {hours + ":" + "" + minutes + ":" + "" + seconds}
             <div>
                 <button
-                    className={`${startCount ? "hidden" : "block"}`}
-                    onClick={() => handleCounter()} type="button">Start Timer</button>
+                    className={`${stopCounter ? "hidden" : "block"}`}
+                    onClick={startTimer} type="button">{seconds > 0 ? "Continue" : "Start Timer"}</button>
                 <button
-                    className={`${startCount ? "block" : "hidden"}`}
-                    onClick={() => {
-                        setStopCounter(true)
-                    }} type="button">Stop Timer</button>
+                    className={`${stopCounter ? "block" : "hidden"}`}
+                    onClick={stopTimer} type="button">Stop Timer</button>
             </div>
-            <button onClick={() => {
-                setSecounds(0)
-                setMinutes(0)
-                setHours(0)
-                setDays(0)
-                setStopCounter(true)
-                setStartCount(false)
-            }}>Reset</button>
+            <button onClick={resetCounter}>Reset</button>
         </div>
     )
 }
